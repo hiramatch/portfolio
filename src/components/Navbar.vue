cf2996b37a24d7fdb4f879e1638ff98181eb6898
hiramatch <hiramatch@gmail.com>
2018-12-06 21:54:41 +0900

    Mod navbar-brand to add link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-    <span class="navbar-brand">Hiramatch's portfolio</span>
+    <router-link class="nav-item nav-link navbar-brand" to="/">Shuichi Hirama's portfolio</router-link>
     <div class="collapse navbar-collapse justify-content-end">
       <div class="navbar-nav">
         <router-link class="nav-item nav-link" to="/">Home</router-link>
