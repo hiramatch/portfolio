3f80bb5b07f7e8486a92df5491c029bd1f859323
hiramatch <hiramatch@gmail.com>
2018-12-06 18:22:52 +0900

    Add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/works">Works</router-link> |
-      <router-link to="/contact">Contact</router-link>
-    </div>
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
+<script>
+  import Navbar from '@/components/Navbar.vue'
 
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -17,6 +20,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+    <span class="navbar-brand">Hiramatch's portfolio</span>
+    <div class="collapse navbar-collapse justify-content-end">
+      <div class="navbar-nav">
+        <router-link class="nav-item nav-link" to="/">Home</router-link>
+        <router-link class="nav-item nav-link" to="/about">About</router-link>
+        <router-link class="nav-item nav-link" to="/works">Works</router-link>
+        <router-link class="nav-item nav-link" to="/contact">Contact</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
