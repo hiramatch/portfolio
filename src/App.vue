c45e9407996db420e4797e27b47fd41ad6b5a3af
hiramatch <hiramatch@gmail.com>
2018-12-09 21:03:59 +0900

    Add About page content

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,10 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+h1.title {
+  text-align: center;
+  margin-bottom: 10px;
 }
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div class="about">
+  <h1 class="title">自己紹介</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <img src="../assets/workstation-336369_640.jpg" width="400px" alt="">
+      </div>
+      <div class="col-sm">
+        <dl>
+        <dt>名前</dt>
+        <dd>平間 秀一  (ひらま しゅういち)</dd>
+        <dt>住まい</dt>
+        <dd>東京</dd>
+        <dt>SNS</dt>
+        <dd>
+          <ul>
+           <li>email: <a href="mailto:hiramatch@gmai.com">hiramatch@gmail.com</a></li>
+           <li>Twitter: <a href="https://twitter.com/hiramatch">@hiramatch</a></li>
+           <li>GitHub: <a href="https://github.com/hiramatch">hiramatch</a></li>
+           <li>blog: <a href="https://hiramatch.hatenablog.com/">hiramatch's blog</a></li>
+          </ul>
+        </dd>
+        </dl>
+        </div>
+      <div class="col-sm">
+      </div>
+    </div>
   </div>
+</div>
 </template>
+<style>
+dd {
+  margin-left: 20px;
+}
+</style>
