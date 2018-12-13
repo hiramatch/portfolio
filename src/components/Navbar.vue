d7f4e121ec88d5814bbd276cd7094d4f268b45c5
hiramatch <hiramatch@gmail.com>
2018-12-13 11:59:04 +0900

    Mod Navbar title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-    <router-link class="nav-item nav-link navbar-brand" to="/">Shuichi Hirama's portfolio</router-link>
+    <router-link class="nav-item nav-link navbar-brand" to="/">portfolio</router-link>
     <div class="collapse navbar-collapse justify-content-end">
       <div class="navbar-nav">
         <router-link class="nav-item nav-link" to="/">Home</router-link>
