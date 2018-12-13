d7e9801241a3f370fdf4433a79d98fe8a8e73c59
hiramatch <hiramatch@gmail.com>
2018-12-13 11:58:39 +0900

    Mod Top page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="jumbotron jumbotron-fluid bg-info">
     <div class="container">
-      <h1 class="display-4">Welcome to Shuichi Hirama's portfolio</h1>
+      <h1 class="display-4">Welcome to My portfolio</h1>
       <p class="lead">This site is made with Vue.js, Bootstrap.</p>
     </div>
   </div>
