d58832a76218db1c1e921b707d2f4c4dce74d3a8
hiramatch <hiramatch@gmail.com>
2018-12-18 09:47:48 +0900

    Navbar モバイル対応

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
     <router-link class="nav-item nav-link navbar-brand" to="/">portfolio</router-link>
-    <div class="collapse navbar-collapse justify-content-end">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
       <div class="navbar-nav">
         <router-link class="nav-item nav-link" to="/">Home</router-link>
         <router-link class="nav-item nav-link" to="/about">About</router-link>
