3412d105793bb7e13da060b692208dd410841190
hiramatch <hiramatch@gmail.com>
2018-12-18 09:50:53 +0900

    Mod works page

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -4,17 +4,20 @@
    <article class="" v-for="work in works">
     <section>
       <div class="container">
-        <div class="row">
+        <div class="row row-works">
             <div class="col-sm-4">
-              <img src="../assets/portfolio.png" width="300px" alt="">
+              <img :src="work.img_src" width="100%" alt="">
             </div>
             <div class="col-sm-6">
-              <h5>{{ work.title }}</h5>
               <dl>
+              <dt>タイトル</dt>
+              <dd>{{ work.title }}</dd>
               <dt>説明</dt>
               <dd>{{ work.description}}</dd>
               <dt>制作年</dt>
               <dd>{{ work.date }}</dd>
+              <dt>技術</dt>
+              <dd>{{ work.skill }}</dd>
               <dt>URL</dt>
               <dd><a :href=work.url>{{ work.title }}</a></dd>
               </dl>
@@ -27,15 +30,17 @@
 
 </template>
 <script>
+import Data from '../assets/data/data.json'
 export default {
   data () {
     return {
-      works: [
-        { title: 'Portfolio1', description: 'description1' ,date: '2018年', url: 'www.example.com' },
-        { title: 'Portfolio2', description: 'description2' ,date: '2017年', url: 'www.example.com' },
-        { title: 'Portfolio3', description: 'description3' ,date: '2016年', url: 'www.example.com' }
-      ]
+      works: Data.works
     }
   }
 }
 </script>
+<style>
+.row-works {
+  margin-top: 40px;
+}
+</style>
