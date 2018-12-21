d853965b9567962c4f96f76da8feb5a5510e0b33
hiramatch <hiramatch@gmail.com>
2018-12-21 22:03:02 +0900

    Worksページの文言修正(技術)
    問い合わせページの入力フォームを一旦とりやめ

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="title">
     <h1>お問い合わせ</h1>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-6 offset-sm-3">
-           Coming soon
-        </div>
-      </div>
-    </div>
+    <p>coming soon</p>
   </div>
 </template>
