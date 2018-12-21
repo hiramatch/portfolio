0231fe199bbe2191b4a31ae9358c9d5c4bae2d84
hiramatch <hiramatch@gmail.com>
2018-12-21 22:03:48 +0900

    Google Formsを利用してお問い合わせフォームを作成

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="title">
-    <h1>お問い合わせ</h1>
-    <p>coming soon</p>
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScV5_3p6WqBtRrXGFu4-sLwjtN5qgkbU5QVEOIgGdRCZiY0WQ/viewform?embedded=true" width="640" height="751" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
   </div>
 </template>
