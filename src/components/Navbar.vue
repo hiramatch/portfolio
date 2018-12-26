2b05bdee8433ea1616ae0014aac4af72bbb71a01
hiramatch <hiramatch@gmail.com>
2018-12-26 12:34:12 +0900

    Navbarをリファクタリング

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
     <router-link class="nav-item nav-link navbar-brand" to="/">portfolio</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler-icon navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
       <div class="navbar-nav">
