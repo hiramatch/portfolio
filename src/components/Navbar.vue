0b0e1cde22e70718534b8342a2a60645967660cc
hiramatch <hiramatch@gmail.com>
2019-03-04 18:54:29 +0900

    ナビゲーションバーデザイン変更

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <router-link class="nav-item nav-link navbar-brand" to="/">portfolio</router-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -9,8 +9,12 @@
         <router-link class="nav-item nav-link" to="/">Home</router-link>
         <router-link class="nav-item nav-link" to="/about">About</router-link>
         <router-link class="nav-item nav-link" to="/works">Works</router-link>
-        <router-link class="nav-item nav-link" to="/contact">Contact</router-link>
       </div>
     </div>
   </nav>
 </template>
+<style>
+  .navbar {
+    background-color: #fefefe;
+  }
+</style>
